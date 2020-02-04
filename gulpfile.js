cda0d382e0402add6e67d3e1efb34133f38a983a
Katrine-Marie Burmeister <fjordscene@gmail.com>
2020-02-04 21:24:30 +0100

    Set up Gulp with plugins

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+var minifyjs = require('gulp-js-minify');
+
+gulp.task('default', () =>
+    gulp.src('src/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('instascrape.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(minifyjs())
+        .pipe(gulp.dest('dist'))
+);
