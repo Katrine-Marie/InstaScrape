7799d60dadf3cd712720436067d81467140fff8a
Katrine-Marie Burmeister <fjordscene@gmail.com>
2020-02-05 00:35:48 +0100

    Added Object.assign polyfill

--- src/instascrape.js
+++ src/instascrape.js
@@ -9,7 +9,35 @@
  *
  */
 
+ // Added for IE11 compatibility
+ if (typeof Object.assign != 'function') {
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) {
+      'use strict';
+      if (target == null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
 
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) {
+          for (var nextKey in nextSource) {
+
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
 
 var InstaScrape = (function(){
     var defaults = {
