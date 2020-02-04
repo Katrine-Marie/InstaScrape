6aca088e4fa7f6086b910857281236a7581e7888
Katrine-Marie Burmeister <fjordscene@gmail.com>
2020-02-04 21:38:54 +0100

    Configured ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 var minifyjs = require('gulp-js-minify');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', () =>
     gulp.src('src/**/*.js')
         .pipe(sourcemaps.init())
+        .pipe(eslint('config.json'))
+        .pipe(eslint.format())
         .pipe(babel())
         .pipe(concat('instascrape.js'))
         .pipe(sourcemaps.write('.'))
